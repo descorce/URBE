222ecadc69890ea31615340ee59a7cfe167358dd
David Escorce <davidescorce03@gmail.com>
2021-07-01 01:09:02 -0400

    Update menu.js

--- GenshinImpact/javascript/menu.js
+++ GenshinImpact/javascript/menu.js
@@ -12,21 +12,3 @@ document.querySelector('.menu').addEventListener('click',function(){
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.menu-screen').classList.remove('active');
 });
-
-let links = document.querySelectorAll('.menu-screen a');
-
-
-links.forEach(link => {
-    link.addEventListener('click',function(event){
-        document.querySelector('.menu-screen').classList.remove('active');
-
-        let paths = this.href.split("/");
-
-        const selector = paths[paths.length - 1];
-
-        if(window.scrollTo) event.preventDefault();
-        scrollToElement(document.querySelector(selector));
-        
-        return !!window.scrollTo;
-    });
-});
